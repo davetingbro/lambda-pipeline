f97b34dd2c2f47a6b53d2080788ed0a28708351d
davetingbro <davidting3250@gmail.com>
2020-06-10 21:29:48 +1200

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var time = require("time");
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+const time = require("time");
 
-exports.handler = (event, context, callback) => {
-  var currentTime = new time.Date();
-  currentTime.setTimezone("America/Los_Angeles");
+exports.handler = async (event, context, callback) => {
+  const params = {
+    Bucket: "david-ting-hello-world",
+  };
+
+  const s3Objects = (await s3.listObjectsV2(params).promise()).Contents;
+  const keys = s3Objects.map((obj) => obj["Key"]);
+  const names = keys.join(", ");
+
+  const currentTime = new time.Date();
+
   callback(null, {
     statusCode: "200",
-    body: "The time in Los Angeles is: " + currentTime.toString(),
+    body: `Hi ${names}  - the time on the server is ${currentTime}`,
   });
 };
